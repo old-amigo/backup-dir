97e5c44acd1f5505903c8cdb602ecad27603c45c
Куруленко Дмитрий <dmitry.gwork@gmail.com>
2022-03-24 21:55:08 +0300

    feat: если директория назначения не существует, скрипт ее создает
    переработан механизм проверки директорий

--- backup-dir.sh
+++ backup-dir.sh
@@ -1,26 +1,44 @@
 #!/bin/bash
 
+function printParamsHelp {
+  echo '---------------------------------------------'
+  echo 'Скрипт принимает 2 параметра:'
+  echo '  1) путь к целевой директории'
+  echo '  2) путь к директории для сохранения бэкапа'
+  echo 'Пожалуйста, проверьте параметры'
+}
+
 source="$1"
 destination="$2"
 
-echo "$1" "$2"
-echo -n "Проверка входящих параметров...    "
+echo "Проверка входящих параметров...    "
 
-if
-  ! [ -d "$source" ] |
-    [ -d "$destination" ] |
-    [ "$3" == '' ]
-then
-  echo 'FAIL (on line 8)'
-  echo 'Скрипт принимает 2 параметра:'
-  echo '    1) путь к директории источника'
-  echo '    2) путь к директории для сохранения бэкапа'
-  echo 'Пожалуйста, проверьте параметры'
+echo -n "  Проверка целевой директории - $source ..."
+if [ -d "$source" ]; then
+  echo "    OK"
+else
+  echo "    FAIL (on line 17)"
+  printParamsHelp
   exit
 fi
 
-echo 'OK'
+echo -n "  Проверка директории для сохранения бэкапа - $destination"
+if [ -d "$destination" ]; then
+  echo "    OK"
+else
+  echo ""
+  echo  -n "  Директория $destination не существует. Создание..."
+  /usr/bin/mkdir -p "$destination"
+  echo "    OK"
+fi
 
-echo -n "Создание архива директории...    "
+if [ "$3" != "" ]; then
+    echo "  Обнаружены лишние параметры...    FAIL (on line 35)"
+    printParamsHelp
+    exit
+fi
+
+
+echo -n "Создание архива целевой директории...    "
 tar -czf "$destination/backup.tar.gz" "$source" 2>/dev/null
 echo "OK"
