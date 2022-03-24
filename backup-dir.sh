f42633598b8659642a259607e629da0b83478783
Куруленко Дмитрий <dmitry.gwork@gmail.com>
2022-03-24 20:49:11 +0300

    feat: backup-dir.sh проверяет аргументы пользователя и выводит в stdout сообщения об ошибках в параметрах

--- backup-dir.sh
+++ backup-dir.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
-echo -n "Создание архива директории $2 в $1... "
-tar -czf "$1" "$2" 2> /dev/null
+source="$1"
+destination="$2"
+
+echo "$1" "$2"
+echo -n "Проверка входящих параметров...    "
+
+if
+  ! [ -d "$source" ] |
+    [ -d "$destination" ] |
+    [ "$3" == '' ]
+then
+  echo 'FAIL (on line 8)'
+  echo 'Скрипт принимает 2 параметра:'
+  echo '    1) путь к директории источника'
+  echo '    2) путь к директории для сохранения бэкапа'
+  echo 'Пожалуйста, проверьте параметры'
+  exit
+fi
+
+echo 'OK'
+
+echo -n "Создание архива директории...    "
+tar -czf "$destination/backup.tar.gz" "$source" 2>/dev/null
 echo "OK"
